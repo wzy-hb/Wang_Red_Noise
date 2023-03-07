12945fb226a447dc405164d1828b086a93b216d0
wzy-hb <wzy1781768693@163.com>
2023-03-07 17:18:12 +0800

    change RedNoise_delay_block

--- enterprise_extensions/precession_model.py
+++ enterprise_extensions/precession_model.py
@@ -1,17 +1,13 @@
 ## 编写进动模型的红噪声函数和调用函数
 import numpy as np
-
+from enterprise.signals import (deterministic_signals, parameter, signal_base,
+                                utils)
 
 @signal_base.function
 def RedNoise_delay(t, t0, P, K, O, X, k):
 
     """
     ##定义红噪声函数
-
-    @signal_base.function不明白, 
-    RedNoise_delay函数作为signal_base.function函数的入参
-    相当于执行signal_base.function(RedNoise_delay(,,,)),
-    需要替换@signal_base.function吗?
 
     :param a1: 
         第一次谐波的振幅
@@ -49,14 +45,49 @@ def RedNoise_delay(t, t0, P, K, O, X, k):
     return res
 
 
-def RedNoise_delay_block():
+def RedNoise_delay_block(name='RedNoise',
+                         t_lower=1, t_upper=10,
+                         t0_lower=1, t0_upper=10,
+                         P_lower=1, P_upper=10,
+                         K_lower=1, K_upper=10,
+                         O_lower=1, O_upper=10,
+                         X_lower=1, X_upper=10,
+                         k_lower=1, k_upper=10):
 
     """
     调用RedNoise_delay函数
     
     """
 
-    # 红噪声残差
-    RedNoise_d = RedNoise_delay(t,t0,P,K,O,X,k)
+    # RedNoise_delay parameters
+    t_name = '{}_log10_t'.format(name)
+    log10_t_RedNoise = parameter.Uniform(t_lower, t_upper)(t_name)
 
-    return RedNoise_d
+    t0_name = '{}_log10_t0'.format(name)
+    log10_t0_RedNoise = parameter.Uniform(t0_lower, t0_upper)(t0_name)
+
+    P_name = '{}_log10_P'.format(name)
+    log10_P_RedNoise = parameter.Uniform(P_lower, P_upper)(P_name)
+
+    K_name = '{}_log10_K'.format(name)
+    log10_K_RedNoise = parameter.Uniform(K_lower, K_upper)(K_name)
+
+    O_name = '{}_log10_O'.format(name)
+    log10_O_RedNoise = parameter.Uniform(O_lower, O_upper)(O_name)
+
+    X_name = '{}_log10_X'.format(name)
+    log10_X_RedNoise = parameter.Uniform(X_lower, X_upper)(X_name)
+
+    k_name = '{}_log10_k'.format(name)
+    log10_k_RedNoise = parameter.Uniform(k_lower, k_upper)(k_name)
+
+
+    # 红噪声残差
+    RedNoise_d = RedNoise_delay(log10_t=log10_t_RedNoise, log10_t0=log10_t0_RedNoise, 
+                                log10_P=log10_P_RedNoise, log10_K=log10_K_RedNoise, 
+                                log10_O=log10_O_RedNoise, log10_X=log10_X_RedNoise, 
+                                log10_k=log10_k_RedNoise)
+    
+    RedNoise = deterministic_signals.Deterministic(RedNoise_d, name=name)
+
+    return RedNoise
